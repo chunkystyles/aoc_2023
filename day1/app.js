a1d3cab87210e2f569e43b2fb460a91124fc5d2c
Allen Archer <chunkystyles@gmail.com>
2023-12-02 07:44:27 -0500

    Day 1 part 2 finished

--- day1/app.js
+++ day1/app.js
@@ -1,6 +1,8 @@
 import { getInput } from '../utils/utils.js'
 
-async function day1() {
+const numberWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+async function part1() {
     const input = await getInput(1);
     const split = input.split('\n');
     let total = 0;
@@ -34,4 +36,72 @@ async function day1() {
     return total;
 }
 
-day1().then(result => console.log(result));
+async function part2() {
+    const input = await getInput(1);
+    const split = input.split('\n');
+    let total = 0;
+    split.forEach(line => {
+        let firstFound = false;
+        let secondFound = false;
+        let isAlpha = false;
+        let firstDigit = '';
+        let secondDigit = '';
+        let word = '';
+        let filteredWords;
+        for (let i = 0; i < line.length; i++) {
+            const char = line[i];
+            let digit;
+            let digitFound = false;
+            if (char >= '0' && char <= '9') {
+                if (isAlpha) {
+                    isAlpha = false;
+                    word = '';
+                }
+                digit = char;
+                digitFound = true;
+            } else {
+                if (isAlpha) {
+                    word += char;
+                    filteredWords = filteredWords.filter(s => s.startsWith(word));
+                    if (filteredWords.length === 0) {
+                        filteredWords = numberWords.filter(s => s.startsWith(char));
+                        i -= (word.length - 1);
+                        isAlpha = false;
+                        word = '';
+                    } else if (filteredWords.length === 1 && word === filteredWords[0]) {
+                        i -= (word.length - 1);
+                        digit = (numberWords.indexOf(word) + 1).toString();
+                        digitFound = true;
+                        isAlpha = false;
+                        word = '';
+                    }
+                } else {
+                    filteredWords = numberWords.filter(s => s.startsWith(char));
+                    if (filteredWords.length > 0) {
+                        isAlpha = true;
+                        word = char;
+                    }
+                }
+            }
+            if (digitFound) {
+                if (!firstFound) {
+                    firstDigit = digit;
+                    firstFound = true;
+                } else {
+                    secondDigit = digit;
+                    secondFound = true;
+                }
+            }
+        }
+        if (!firstFound) {
+            console.log(`ERROR on line ${line}`);
+        } else if (!secondFound) {
+            total += Number(firstDigit + firstDigit);
+        } else {
+            total += Number(firstDigit + secondDigit);
+        }
+    });
+    return total;
+}
+
+part2().then(result => console.log(result));
