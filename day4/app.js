64e38e5eac5d7925c2fd7af412241f1c0c1fc239
Allen Archer <chunkystyles@gmail.com>
2023-12-04 09:54:25 -0500

    Day 4 part 2 finished

--- day4/app.js
+++ day4/app.js
@@ -7,14 +7,29 @@ async function part1() {
     }).reduce((sum, x) => sum + x, 0);
 }
 
+async function part2() {
+    const cards = parseInput(await getInput(4));
+    let total = 0;
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        total += card.copies;
+        const matches = card.card.filter(number => card.winningNumbers.has(number)).length;
+        for (let j = 1; j + i < cards.length && j <= matches; j++) {
+            cards[j + i].copies += card.copies;
+        }
+    }
+    return total;
+}
+
 function parseInput(input) {
     return input.split('\n').filter(string => string).map(line => {
         const parts = line.split(': ')[1].split(' | ');
         return {
             card: parts[1].split(' ').filter(string => string).map(number => Number(number)),
-            winningNumbers: new Set(parts[0].split(' ').filter(string => string).map(number => Number(number)))
+            winningNumbers: new Set(parts[0].split(' ').filter(string => string).map(number => Number(number))),
+            copies: 1
         }
     });
 }
 
-part1().then(result => console.log(result));
+part2().then(result => console.log(result));
