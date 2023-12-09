4636bd0bfedd9c1aecdd7ae9cb361289523156f7
Allen Archer <chunkystyles@gmail.com>
2023-12-09 10:24:03 -0500

    Day 9 part 2 finished

--- day9/app.js
+++ day9/app.js
@@ -25,8 +25,33 @@ async function part1() {
     return total;
 }
 
+async function part2() {
+    const histories = parseInput(await getInput(9));
+    for (const history of histories) {
+        while (true) {
+            const newLevel = [];
+            const currentLevel = history[history.length - 1];
+            for (let i = 0; i < currentLevel.length - 1; i++) {
+                newLevel.push(currentLevel[i + 1] - currentLevel[i]);
+            }
+            history.push(newLevel);
+            if (newLevel.filter(value => value !== 0).length === 0) {
+                break;
+            }
+        }
+    }
+    let total = 0;
+    for (const history of histories) {
+        for (let i = history.length - 1; i > 0; i--) {
+            history[i - 1].unshift(history[i - 1][0] - history[i][0]);
+        }
+        total += history[0][0];
+    }
+    return total;
+}
+
 function parseInput(input) {
     return input.split('\n').filter(string => string).map(string => string.split(' ').map(string => Number(string))).map(history => [history]);
 }
 
-part1().then(result => console.log(result));
+part2().then(result => console.log(result));
