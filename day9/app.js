f3502bd316cf7349028d6b9b7ab2e9e7fc52a483
Allen Archer <chunkystyles@gmail.com>
2023-12-09 10:22:56 -0500

    Day 9 part 1 finished

---
+++ day9/app.js
@@ -0,0 +1,32 @@
+import { getInput, writeJsonFile } from '../utils/utils.js'
+
+async function part1() {
+    const histories = parseInput(await getInput(9));
+    for (const history of histories) {
+        while (true) {
+            const newLevel = [];
+            const currentLevel = history[history.length - 1];
+            for (let i = 0; i < currentLevel.length - 1; i++) {
+                newLevel.push(currentLevel[i + 1] - currentLevel[i]);
+            }
+            history.push(newLevel);
+            if (newLevel.filter(value => value !== 0).length === 0) {
+                break;
+            }
+        }
+    }
+    let total = 0;
+    for (const history of histories) {
+        for (let i = history.length - 1; i > 0; i--) {
+            history[i - 1].push(history[i][history[i].length - 1] + history[i - 1][history[i - 1].length - 1]);
+        }
+        total += history[0][history[0].length - 1];
+    }
+    return total;
+}
+
+function parseInput(input) {
+    return input.split('\n').filter(string => string).map(string => string.split(' ').map(string => Number(string))).map(history => [history]);
+}
+
+part1().then(result => console.log(result));
