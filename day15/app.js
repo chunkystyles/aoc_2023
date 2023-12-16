be7d9e4d2e8fde26aee10d6b81ce736aab26ca9e
Allen Archer <chunkystyles@gmail.com>
2023-12-16 12:34:06 -0500

    Day 15 part 2 finished

--- day15/app.js
+++ day15/app.js
@@ -1,4 +1,5 @@
 import { getInput } from '../utils/utils.js'
+const boxes = [];
 
 async function part1() {
     const input = parseInput(await getInput(15));
@@ -21,4 +22,52 @@ function parseInput(input) {
     return input.replaceAll('\n', '').split(',').filter(string => string);
 }
 
-part1().then(result => console.log(result));
+async function part2() {
+    const instructions = parseInput(await getInput(15));
+    for (let i = 0; i < 256; i++){
+        boxes.push([]);
+    }
+    for (const instruction of instructions) {
+        if (instruction.includes('-')) {
+            const label = instruction.substring(0, instruction.length - 1);
+            const index = hash(label);
+            for (let i = 0; i < boxes[index].length; i++) {
+                if (boxes[index][i].label === label) {
+                    boxes[index].splice(i, 1);
+                    break;
+                }
+            }
+        } else {
+            const split = instruction.split('=');
+            const label = split[0];
+            const power = Number(split[1]);
+            const index = hash(label);
+            let found = false;
+            for (let i = 0; i < boxes[index].length; i++) {
+                if (boxes[index][i].label === label) {
+                    found = true;
+                    boxes[index][i].power = power;
+                    break;
+                }
+            }
+            if (!found) {
+                boxes[index].push({label: label, power: power});
+            }
+        }
+    }
+    return calculateFocusingPower();
+}
+
+function calculateFocusingPower() {
+    let total = 0;
+    for (let i = 0; i < boxes.length; i++) {
+        for (let j = 0; j < boxes[i].length; j++) {
+            if (boxes[i][j]) {
+                total += (i + 1) * (j + 1) * boxes[i][j].power;
+            }
+        }
+    }
+    return total;
+}
+
+part2().then(result => console.log(result));
